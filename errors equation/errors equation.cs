724e1797198cc138ad82b105ce9fe4a5d5cc71b1
高涛 <gaotaomo@live.com>
2016-06-10 22:53:00 +0800

    Add project files.

---
+++ errors equation/errors equation.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using shortest_path;
+using shortest_path;
+
+namespace errors_equation
+{
+    public class errors_equation
+    {
+        private struct point
+        {
+            public int index;
+            public string name;
+            public double Hexact;
+            public double Hcirca;
+        }
+
+        private struct observation
+        {
+            public string starName;
+            public string stopName;
+            public double Height;
+            public double length;
+        }
+
+        private struct polynomial
+        {
+            public int[] single;
+            public double constant;
+        }
+            
+        private point[] my_point;
+        private observation[] my_observation;
+        private polynomial[] my_polynomial;
+
+        private int pathNum;
+        private int pointNum;
+        private int pointNum_K;
+
+        private string[] pointName;
+        private double[] ponintHknown;
+
+        private string[] singlePath;
+        private string[,] mixPath;
+
+        private double[] singleDistance;
+        private double[,] mixDistance;
+
+        private int[] indexMix;
+        private string[] pathMix;
+
+        private short[,] singlePolynomial;
+        private double[] constantPolynomial;
+
+        private int[,] MatrixB;
+        private double[,] MatrixL;
+        private double[,] MatrixP;
+
+        public errors_equation(int my_pathNum, int my_pointNum, int my_pointNum_K, string[] my_pointName, double[] my_ponintHknown)
+        {
+            pathNum = my_pathNum;
+            pointNum = my_pointNum;
+            pointNum_K = my_pointNum_K;
+
+            my_point = new point[pointNum];
+            my_observation = new observation[pathNum];
+            my_polynomial = new polynomial[pathNum];
+
+            singlePath = new string[pointNum];
+            Array.Clear(singlePath, 0, my_pointNum);
+            mixPath = new string[pointNum - pointNum_K, pointNum_K];
+
+            singleDistance = new double[pointNum];
+            Array.Clear(singleDistance, 0, my_pointNum);
+            mixDistance = new double[pointNum - pointNum_K, pointNum_K];
+
+            indexMix = new int[(pointNum - my_pointNum_K)];
+            Array.Clear(indexMix, 0, pointNum - pointNum_K);
+            pathMix = new string[(pointNum - pointNum_K)];
+            Array.Clear(pathMix, 0, pointNum - pointNum_K);
+
+            pointName = new string[pointNum];
+            Array.Copy(my_pointName, pointName, 0);
+            ponintHknown = new double[pointNum];
+            Array.Copy(my_ponintHknown, ponintHknown, 0);
+
+            singlePolynomial = new short[pathNum, pointNum - pointNum_K];
+            constantPolynomial = new double[pathNum];
+            Array.Clear(constantPolynomial, 0, pathNum);
+
+            MatrixB = new int[pathNum, pointNum - pointNum_K];
+            MatrixL = new double[1,pathNum];
+            MatrixP = new double[pathNum, pathNum];
+
+            for (int i = 0; i < pointNum; i++)
+            {
+                my_point[i].index = i;
+                my_point[i].name = my_pointName[i];
+
+                pointName[i] = my_pointName[i];
+
+                my_polynomial[i].constant = 0.0;
+                my_polynomial[i].single = new int[pointNum - pointNum_K];
+
+                for (int j = 0; j < pointNum - pointNum_K; j++)
+                {
+                    my_polynomial[i].single[j] = 0;
+                    singlePolynomial[i, j] = 0;
+                }
+
+                if (i < pointNum_K)
+                {
+                    ponintHknown[i] = my_ponintHknown[i];
+                    my_point[i].Hexact = ponintHknown[i];
+                    my_point[i].Hcirca = ponintHknown[i];
+                }
+                else
+                {
+                    my_point[i].Hexact = 0;
+                    my_point[i].Hcirca = 0;
+                }
+            }
+        }
+
+        public void createObservation(int i, string starName, string stopName, double Height, double length)
+        {
+            my_observation[i].starName = starName;
+            my_observation[i].stopName = stopName;
+            my_observation[i].Height = Height;
+            my_observation[i].length = length;
+        }
+
+        public void crateErrorsEquation()
+        {
+            shortest_path.shortest_path my_shortest_path = new shortest_path.shortest_path(pathNum, pointNum, pointNum_K, pointName);
+
+            for (int i = 0; i < pathNum; i++)
+            {
+                my_shortest_path.createObservation(i, my_observation[i].starName, my_observation[i].stopName, my_observation[i].length);
+            }
+
+            my_shortest_path.createGraph();
+
+            for (int i = 0; i < pointNum - pointNum_K; i++)
+            {
+                my_shortest_path.Dijkstra(i + pointNum_K);
+
+                singlePath = my_shortest_path.getmixPath();
+                singleDistance = my_shortest_path.getmixDistance();
+
+                for (int j = 0; j < pointNum_K; j++)
+                {
+                    mixPath[i,j] = singlePath[j];
+                    mixDistance[i,j] = singleDistance[j];
+
+                    if (mixDistance[i, j] != 0.0)
+                    {
+                        Console.Write("mixPath[{0}]:{1}    ", j, mixPath[i, j]);
+                        Console.Write("mixDistance[{0}]:{1}", j, mixDistance[i, j]);
+
+                        Console.Write("\n");
+                    }
+                }
+                Console.WriteLine("/////////////////////////////////////////////////////////////");
+                Array.Clear(singlePath, 0, pointNum);
+                Array.Clear(singleDistance, 0, pointNum);
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < pointNum - pointNum_K; i++)
+            {
+                double tempMix = mixDistance[i, 0];
+                indexMix[i] = 0;
+                if (pointNum_K != 1)
+                {
+                    for (int j = 0; j < pointNum_K - 1; j++)
+                    {
+                        if (mixDistance[i, j + 1] < tempMix)
+                        {
+                            indexMix[i] = j + 1;
+                            tempMix = mixDistance[i, j + 1];
+                        }
+                    }
+                }
+                Console.WriteLine("indexMix[{0}]：{1}", i, indexMix[i]);
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < pointNum - pointNum_K; i++)
+            {
+                for (int j = 0; j < pointNum_K; j++)
+                {
+                    if (j == indexMix[i])
+                        pathMix[i] = mixPath[i,j];
+                }
+                Console.WriteLine("pathMix[{0}]：{1}", i, pathMix[i]);
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < pointNum - pointNum_K; i++)
+            {
+                int[] digitPath = getDigitPath(pathMix[i]);
+
+                for (int j = 0; j < digitPath.Length; j++)
+                {
+                    Console.WriteLine("digitPath[{0}]：{1}", j, digitPath[j]);
+                }
+
+                double Height = 0.0;
+                my_point[i + pointNum_K].Hcirca = my_point[digitPath[digitPath.Length - 1]].Hexact;
+
+                for (int j = digitPath.Length - 1; j > 0; j--)
+                {
+                    double singleHeight = 0.0;
+                    for (int k = 0; k < pathNum; k++)
+                    {
+                        if (my_point[digitPath[j]].name == my_observation[k].starName && my_point[digitPath[j - 1]].name == my_observation[k].stopName)
+                        {
+                            singleHeight = my_observation[k].Height;
+                            k = pathNum;
+                        }
+                        else if (my_point[digitPath[j]].name == my_observation[k].stopName && my_point[digitPath[j - 1]].name == my_observation[k].starName)
+                        {
+                            singleHeight = -(my_observation[k].Height);
+                            k = pathNum;
+                        }
+                    }
+                    Height += singleHeight;
+                }
+
+                my_point[i + pointNum_K].Hcirca += Height;
+
+                Console.WriteLine("my_point[{0}]：{1}", i + pointNum_K, my_point[i + pointNum_K].Hcirca);
+
+                Console.WriteLine("/////////////////////////////////////////////////////////////");
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < pathNum; i++)
+            {
+                int starIndex = Array.IndexOf(pointName, my_observation[i].starName);
+                int stopIndex = Array.IndexOf(pointName, my_observation[i].stopName);
+
+                for (int j = 0; j < pointNum - pointNum_K; j++)
+                {
+                    if (j + pointNum_K == starIndex || j + pointNum_K == stopIndex)
+                    {
+                        if (j + pointNum_K == starIndex)
+                        {
+                            singlePolynomial[i, j] = -1;
+                            //my_polynomial[i].single[j] = -1;
+                        }
+                        else
+                        {
+                            singlePolynomial[i, j] = 1;
+                            //my_polynomial[i].single[j] = 1;
+                        }
+                    }
+                    //Console.WriteLine("my_polynomial[{0}].single[{1}]： {2}", i, j, my_polynomial[i].single[j]);
+                    Console.WriteLine("singlePolynomial[{0}][{1}]： {2}", i, j, singlePolynomial[i, j]);
+                }
+                //my_polynomial[i].constant = -1000 * Math.Round((my_point[stopIndex].Hcirca - my_point[starIndex].Hcirca - my_observation[i].Height), 3);
+                constantPolynomial[i] = -1000*(my_point[stopIndex].Hcirca - my_point[starIndex].Hcirca - my_observation[i].Height);
+
+                //Console.WriteLine("my_polynomial[{0}].constant： {1}", i, my_polynomial[i].constant);
+                Console.WriteLine("constantPolynomial[{0}]： {1}", i, constantPolynomial[i].ToString("0"));
+
+                Console.WriteLine("/////////////////////////////////////////////////////////////");
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < pathNum; i++)
+                for (int j = 0; j < pointNum - pointNum_K; j++)
+                    MatrixB[i,j] = singlePolynomial[i, j];
+
+            for (int i = 0; i < pathNum; i++)
+                MatrixL[0,i] = constantPolynomial[i];
+
+            for (int i = 0; i < pathNum; i++)
+                for (int j = 0; j < pathNum; j++)
+                {
+                    if (j == i)
+                    {
+                        MatrixP[i,j] = 1.0/my_observation[i].length;
+                    }
+                    else
+                        MatrixP[i,j] = 0.0;
+                }
+
+        }
+
+        private int[] getDigitPath(string stringPath)
+        {
+            string[] strTemp = stringPath.Trim().Split(',');
+            int l = strTemp.Length;
+            int[] intPath = new int[l];
+            for (int i = 0; i < l; i++)
+            {
+                intPath[i] = Array.IndexOf(pointName, strTemp[i]);
+            }
+            return intPath;
+        }
+
+        public int[,] getMatrixB()
+        {
+            return MatrixB;
+        }
+
+        public double[,] getMatrixL()
+        {
+            return MatrixL;
+        }
+
+        public double[,] getMatrixP()
+        {
+            return MatrixP;
+        }
+    }
+}
---
+++ useErrorsEquation/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using errors_equation;
+using System.IO;
+
+namespace useErrorsEquation
+{
+    class Program
+    {
+        private struct observation
+        {
+            public string Be;
+            public string En;
+            public double H;
+            public double S;
+        }
+
+        private static observation[] my_observation;
+
+        private static string[] Pname;
+
+        private static double[] HKnown;
+
+        private static int[,] MatrixB;
+        private static double[,] MatrixL;
+        private static double[,] MatrixP;
+
+        static void Main(string[] args)
+        {
+            Int16 Hn = 0;
+            Int16 Un = 0;
+            Int16 Kn = 0;
+
+            string strData = String.Empty;
+            string[] strTemp;
+            int dataIdx = 0;
+
+            FileStream aFile = new FileStream("示例数据2.txt", FileMode.Open);
+            StreamReader streamReader = new StreamReader(aFile);
+
+            while (streamReader.Peek() != -1)
+            {
+                dataIdx++;
+
+                //"strData"存储剔除空格逐行读取文档的当前行所有字符，
+                // "strTemp"以数组形式存储当前行，以逗号分隔
+                strData = streamReader.ReadLine().Trim();
+                strTemp = strData.Trim().Split(',');
+
+                if (dataIdx == 1)
+                {
+                    Kn = Convert.ToInt16(strTemp[0]);
+                    Un = Convert.ToInt16(strTemp[1]);
+                    Hn = Convert.ToInt16(strTemp[2]);
+
+                    Pname = new string[Kn + Un];
+                    HKnown = new double[Kn];
+                    my_observation = new observation[Hn];
+
+                    MatrixB = new int[Hn, Un];
+                    MatrixL = new double[1,Hn];
+                    MatrixP = new double[Hn, Hn];
+
+                }
+
+                else if (dataIdx == 2)
+                {
+                    for (int i = 0; i < Kn + Un; i++)
+                    {
+                        Pname[i] = Convert.ToString(strTemp[i]);
+                    }
+                }
+
+                else if (dataIdx == 3)
+                {
+                    for (int i = 0; i < Kn; i++)
+                    {
+                        HKnown[i] = Convert.ToDouble(strTemp[i]);
+                    }
+                }
+
+                else
+                {
+                    my_observation[dataIdx - 4].Be = Convert.ToString(strTemp[0]);
+                    my_observation[dataIdx - 4].En = Convert.ToString(strTemp[1]);
+                    my_observation[dataIdx - 4].H = Convert.ToDouble(strTemp[2]);
+                    my_observation[dataIdx - 4].S = Convert.ToDouble(strTemp[3]);
+                }
+            }
+
+            Console.WriteLine(" 数据读入成功 ！！！ ");
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.WriteLine(" Hn = {0}, Kn = {1}, Un = {2} ", Hn, Kn, Un);
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < Kn + Un; i++)
+            {
+                Console.WriteLine("第 {0} 点 ：{1}", i + 1, Pname[i]);
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < Kn; i++)
+            {
+                Console.WriteLine("第 {0} 点高程 ：{1}", i + 1, HKnown[i].ToString("0.000"));
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            for (int i = 0; i < Hn; i++)
+            {
+                Console.WriteLine("起点 ：{0} 终点 ：{1} 高差：{2} 距离 ：{3}", my_observation[i].Be, my_observation[i].En, my_observation[i].H.ToString("0.0000"), my_observation[i].S.ToString("0.0000"));
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            string[] singlePath = new string[Kn + Un];
+            string[,] mixPath = new string[Kn + Un, Kn + Un];
+
+            double[] singleDistance = new double[Kn + Un];
+            double[,] mixDistance = new double[Kn + Un, Kn + Un];
+
+            errors_equation.errors_equation my_errors_equation = new errors_equation.errors_equation(Hn, Kn + Un, Kn, Pname, HKnown);
+
+            for (int i = 0; i < Hn; i++)
+            {
+                my_errors_equation.createObservation(i, my_observation[i].Be, my_observation[i].En, my_observation[i].H, my_observation[i].S);
+            }
+
+            my_errors_equation.crateErrorsEquation();
+
+            MatrixB = my_errors_equation.getMatrixB();
+
+            MatrixL = my_errors_equation.getMatrixL();
+
+            MatrixP = my_errors_equation.getMatrixP();
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.WriteLine("MatrixB：");
+
+            for (int i = 0; i < Hn; i++)
+            {
+                for (int j = 0; j < Un; j++)
+                {
+                    Console.Write("{0}     ", MatrixB[i,j]);
+                    if (j == Un - 1)
+                    {
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.WriteLine("MatrixL：");
+
+            for (int i = 0; i < Hn; i++)
+            {
+                Console.Write("{0}     ", MatrixL[0,i].ToString("0"));
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.WriteLine("MatrixP：");
+
+            for (int i = 0; i < Hn; i++)
+            {
+                for (int j = 0; j < Hn; j++)
+                {
+                    Console.Write("{0}     ", MatrixP[i, j].ToString("0.000"));
+                    if (j == Hn - 1)
+                    {
+                        Console.WriteLine("\n");
+                    }
+                }
+            }
+            Console.WriteLine("/////////////////////////////////////////////////////////////");
+
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
